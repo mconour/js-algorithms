d3fae9d2570a28b7321223a7acd6b7825f259a9c
Michael Conour <michaelconour@gmail.com>
2020-12-05 10:21:44 -0800

    add solution 2 to palindrome algorithm test

--- StringReversal/stringReversal.js
+++ StringReversal/stringReversal.js
@@ -1,9 +1,3 @@
-/*
-String Reversal:
------------
-Given a string of characters as input, write a function that returns it with the characters reversed.
-*/
-
 // Solution 1
 
 //const strReversal = str => str.split("").reverse().join("");
--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,15 +1,29 @@
+// Solution 1
+
+/*const isPalindrome = str => {
+
+    
+    const cleaned = str.replace(/\W/g, "").toLowerCase();
+    return (cleaned === cleaned.split("").reverse().join(""));
+}*/
+
+
+// Solution 2
+
 const isPalindrome = string => {
-    const cleaned = string.replace(/\W/g, "").toLowerCase();
-
-    return (
-        cleaned ===
-        cleaned
-        .split("")
-        .reverse()
-        .join("")
-    );
+	const validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+	const stringCharacters = string
+		.toLowerCase()
+		.split("")
+		.reduce(
+			(characters, character) =>
+				validCharacters.indexOf(character) > -1
+					? characters.concat(character)
+					: characters,
+			[]
+		);
+	return stringCharacters.join("") === stringCharacters.reverse().join("");
 };
-
 
 
 
